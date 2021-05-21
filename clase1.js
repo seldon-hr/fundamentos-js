0a852fee0814836eb316e47014bd3d9c3189f316
Daniel Romero <daanmaro99june@gmail.com>
2021-05-21 15:42:24 -0500

    Star to Js

---
+++ clase1.js
@@ -0,0 +1,10 @@
+var nombre  = 'Seldon'
+var apellido = 'Romero'
+var edad = 28
+
+edad = '28 años'
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tnego ' + edad )
+
+var peso = 75
