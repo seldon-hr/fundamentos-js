b15f77bdac0099f812501c99c730240d00d446b9
Daniel Romero <daanmaro99june@gmail.com>
2021-05-25 12:43:00 -0500

    Funciones

---
+++ clase4.js
@@ -0,0 +1,10 @@
+var nombre = 'Seldon', edad = 22
+
+function imprimirEdad(n, e){
+    console.log(`${n} tiene ${e} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 20)
+imprimirEdad('Dan',29)
+imprimirEdad(24,'Carl')
