7532292ee084eec80a8fece539a03405671b8a33
Daniel Romero <daanmaro99june@gmail.com>
2021-06-01 18:28:53 -0500

    Arrays

---
+++ clase17.js
@@ -0,0 +1,43 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    altura: 1.72
+}
+
+var alan = {
+    nombre: 'Alan',
+    apellido: 'Perez',
+    altura: 1.86
+}
+
+var martin = {
+    nombre: 'Martin',
+    apellido: 'Gomez',
+    altura: 1.85
+}
+
+var dario = {
+    nombre: 'Dario',
+    apellido: 'Juarez',
+    altura: 1.71
+}
+
+var vicky = {
+    nombre: 'Vicky',
+    apellido: 'Zapata',
+    altura: 1.56
+}
+
+var paula = {
+    nombre: 'Paula',
+    apellido: 'Barros',
+    altura: 1.76
+}
+
+
+var personas = [sacha, alan, martin, dario, vicky, paula]
+
+for( var i = 0; i < personas.length; i++){
+    var persona = personas[i] //Guardamos cada una de las personas por cada índice de cada una.
+    console.log(`${persona.nombre} mide ${persona.altura}mts`)
+}
