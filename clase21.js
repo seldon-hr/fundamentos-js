7623c10146626d35b9d7b22e67c2e5d861b38e08
Daniel Romero <daanmaro99june@gmail.com>
2021-06-02 18:52:27 -0500

    Uso de funciones de js

---
+++ clase21.js
@@ -0,0 +1,20 @@
+function Persona(nombre, apellido, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.altura = altura
+}
+
+const MAYORIA_ALTURA = 1.8
+
+Persona.prototype.soyAlto = function(){
+    this.altura > MAYORIA_ALTURA ? console.log(`${this.nombre} es alto`) : console.log(`${this.nombre} es bajo`)
+}
+
+Persona.prototype.saludar = function(){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
+}
+
+var seldon = new Persona('Seldon', 'Romero', 1.6)
+var arturo = new Persona('Arturo', 'López', 1.9)
+var juan = new Persona ('Juan', 'Barrio', 1.81)
+
