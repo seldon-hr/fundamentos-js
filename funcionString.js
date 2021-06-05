dfaddd97c53cf23c8e047bafe4ba71b2ad3422bf
Daniel Romero <daanmaro99june@gmail.com>
2021-06-05 16:37:29 -0500

    fuction to string

---
+++ funcionString.js
@@ -0,0 +1,9 @@
+const phrase = 'Hoy es un grand día para programar'
+const word = 'programar'
+
+const findWord = function(phrase, word){
+    let newPhrase = phrase.search('programar')
+    return newPhrase
+}
+
+console.log()
