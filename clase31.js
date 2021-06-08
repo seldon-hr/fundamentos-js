bb0e6098e468ee5614c41212fa9a60916b399262
Daniel Romero <daanmaro99june@gmail.com>
2021-06-07 21:50:35 -0500

    Manejando errores de callbacks

---
+++ clase31.js
@@ -0,0 +1,43 @@
+const API_URL = 'https://swapi.dev/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = { crossDomain: true}
+
+
+function obtenerPersonaje(id, callback /* fn o cb otras formas de llamar a esta función */ ) 
+{
+    const url = `${API_URL}${PEOPLE_URL.replace(':id',id)}`  
+    $
+    .get(url, opts, callback)
+    .fail(() => {
+        console.log(`Sucedio un error. No se pudo obtener el personaje ${id}`)
+    })
+}
+
+obtenerPersonaje(1,  function (personaje){
+    console.log(`Hola, yo soy ${personaje.name}`)
+    
+    obtenerPersonaje(2,  function (personaje){
+        console.log(`Hola, yo soy ${personaje.name}`)
+        
+        obtenerPersonaje(3,  function (personaje){
+            console.log(`Hola, yo soy ${personaje.name}`)
+            
+            obtenerPersonaje(4,  function (personaje){
+                console.log(`Hola, yo soy ${personaje.name}`)
+                
+                obtenerPersonaje(5,  function (personaje){
+                    console.log(`Hola, yo soy ${personaje.name}`)
+                    
+                    obtenerPersonaje(6,  function (personaje){
+                        console.log(`Hola, yo soy ${personaje.name}`)
+                        
+                        obtenerPersonaje(7,function (personaje){
+                            console.log(`Hola, yo soy ${personaje.name}`)
+                        })
+                    })
+                })
+            })
+        })
+    })
+})
+
